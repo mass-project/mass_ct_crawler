b8e161bc7aeba0d088e63845ce75e0257db74ae2
Arne-He <arne.hennies@hotmail.de>
2018-03-06 22:23:44 +0100

    wo_timestamps

--- README.md
+++ README.md
@@ -3,22 +3,27 @@ Utility for crawling certificate transparency servers and submitting CT informat
 
 #List of Suitable CT Servers 
 
-Name | Cert Count | Max Block Size | URL | Active
------|------------|----------------|-----|-------
-Google 'Aviator' log | 46466471 | 1024 | ct.googleapis.com/aviator | no
-Google 'Icarus' log | 186657087 | 1024 | ct.googleapis.com/icarus | yes
-Google 'Pilot' log | 206952402 | 1024 | ct.googleapis.com/pilot | yes
-Google 'Rocketeer' log | 201088116 | 1024 | ct.googleapis.com/rocketeer | yes
-Google 'Skydiver' log | 9944215 | 1024 | ct.googleapis.com/skydiver | yes
-DigiCert Log Server | 4384072 | 65 | ct1.digicert-ct.com/log | yes
-DigiCert Log Server 2 | 1565753 | 65 | ct2.digicert-ct.com/log | yes
-Symantec log | 7087084 | 1024 | ct.ws.symantec.com | yes
-Symantec 'Vega' log | 435605 | 1024 | vega.ws.symantec.com | yes
-Symantec 'Sirius' log | 112009 | 1024 | sirius.ws.symantec.com | yes
-Venafi Gen2 CT log | 102606087 | 1001 | ctlog-gen2.api.venafi.com | yes
-Comodo 'Sabre' CT log | 31316390 | 1001 | sabre.ct.comodo.com | yes
-Comodo 'Mammoth' CT log | 42553944 | 1001 | mammoth.ct.comodo.com | yes
+Name | Cert Count | Max Block Size | URL | new Certs per Hour 
+-----|------------|----------------|-----|------
+Google 'Aviator' log | 46466471 | 1024 | ct.googleapis.com/aviator | 0
+Google 'Icarus' log | 186657087 | 1024 | ct.googleapis.com/icarus | 33500
+Google 'Pilot' log | 206952402 | 1024 | ct.googleapis.com/pilot | 53100
+Google 'Rocketeer' log | 201088116 | 1024 | ct.googleapis.com/rocketeer | 32500
+Google 'Skydiver' log | 9944215 | 1024 | ct.googleapis.com/skydiver | 2600
+DigiCert Log Server | 4384072 | 65 | ct1.digicert-ct.com/log | 520
+DigiCert Log Server 2 | 1565753 | 65 | ct2.digicert-ct.com/log | 200
+Symantec log | 7087084 | 1024 | ct.ws.symantec.com | 700
+Symantec 'Vega' log | 435605 | 1024 | vega.ws.symantec.com | 
+Symantec 'Sirius' log | 112009 | 1024 | sirius.ws.symantec.com | 
+Venafi Gen2 CT log | 102606087 | 1001 | ctlog-gen2.api.venafi.com | 0
+Comodo 'Sabre' CT log | 31316390 | 1001 | sabre.ct.comodo.com | 16000
+Comodo 'Mammoth' CT log | 42553944 | 1001 | mammoth.ct.comodo.com | 15000
+Cloudflare 'Nimbus2019' Log| 394333 | | ct.cloudflare.com/logs/nimbus2019 | 10
+Cloudflare 'Nimbus2020' Log| 25845 | | ct.cloudflare.com/logs/nimbus2020 | 10
+Cloudflare 'Nimbus2021' Log| 6996 | | ct.cloudflare.com/logs/nimbus2021 | 10
 
 (updated: 01-27-2018)
+
+Estimated Domain Duplicate Rate: 29%
 
 For more information about known logs, see: https://www.certificate-transparency.org/known-logs
--- ct_crawler/crawler.py
+++ ct_crawler/crawler.py
@@ -26,7 +26,7 @@ except locale.Error:
     pass
 
 DOWNLOAD_QUEUE_SIZE = 40
-MASS_QUEUE_SIZE = 24
+MASS_QUEUE_SIZE = 1000
 
 
 async def mass_worker(parse_results_queue, mass_concurrency):
@@ -42,25 +42,23 @@ def mass(queue):
                                                                   tag_filter_exp='sample-type:domainsample',
                                                                   )
     while True:
-        entries = queue.get()
-        if entries is None:
+        entry = queue.get()
+        if entry is None:
             break
-        print('[{}] Submitting {} Samples to MASS...'.format(os.getpid(), len(entries)))
-        for entry in entries:
-            for i in range(3):
-                try:
-                    s = Sample.create(domain=entry['all_domains'][0], tags=['domain_with_cert'])
-                    scheduled = anal_system_instance.schedule_analysis(s)
-                    scheduled.create_report(
-                        json_report_objects={'domain_report': ('domain_report', entry)},
-                    )
-                    break
-                except requests.HTTPError:
-                    if i == 2:
-                        logging.error('HTTPError while creating a sample.')
+        for i in range(3):
+            try:
+                s = Sample.create(domain=entry['all_domains'][0], tags=['domain_with_cert'])
+                scheduled = anal_system_instance.schedule_analysis(s)
+                scheduled.create_report(
+                    json_report_objects={'domain_report': ('domain_report', entry)},
+                )
+                break
+            except requests.HTTPError:
+                if i == 2:
+                    logging.error('HTTPError while creating a sample.')
 
 
-async def download_worker(session, log_info, work_deque, download_queue, timestamp):
+async def download_worker(session, log_info, work_deque, download_queue, report):
     while True:
         try:
             start, end = work_deque.popleft()
@@ -87,13 +85,14 @@ async def download_worker(session, log_info, work_deque, download_queue, timesta
                                    "log_info": log_info,
                                    "start": start,
                                    "end": end
-                                   }, timestamp))
+                                   }, report))
 
 
 async def retrieve_certificates(loop, download_concurrency, mass_concurrency, time_sec, once, anal_system_instance):
     async with aiohttp.ClientSession(loop=loop, conn_timeout=10) as session:
         while True:
-            urls = get_ctls_from_mass(anal_system_instance)
+            pre = time.time()
+            urls = get_ctls_from_mass()
             ctl_logs = await certlib.retrieve_all_ctls(session)
             for log in ctl_logs:
                 if log['url'] not in urls:
@@ -111,8 +110,12 @@ async def retrieve_certificates(loop, download_concurrency, mass_concurrency, ti
                     logging.error("Failed to connect to CTL! -> {} - skipping.".format(e))
                     continue
 
+                if urls[log['url']]['initial'] is True:
+                    urls[log['url']]['offset'] = log_info['tree_size']
+                create_ctl_report(anal_system_instance, log['url'], log_info['tree_size'])
+
                 try:
-                    await certlib.populate_work(work_deque, log_info, start=0)
+                    await certlib.populate_work(work_deque, log_info, start=urls[log['url']]['offset'])
                 except Exception as e:
                     logging.error("Log needs no update - {}".format(e))
                     continue
@@ -133,8 +136,11 @@ async def retrieve_certificates(loop, download_concurrency, mass_concurrency, ti
                 print('Parsing complete. MASS Queue: {}'.format(parse_results_queue.qsize()))
                 await mass_task
             if not once:
-                logging.info('Completed. Sleeping for {} seconds.'.format(time_sec))
-                time.sleep(time_sec)
+                after = time.time()
+                new = time_sec - (after - pre)
+                logging.info('Completed. Sleeping for {} seconds.'.format(int(new)))
+                if new > 0:
+                    time.sleep(new)
             else:
                 logging.info('Completed.')
                 break
@@ -161,7 +167,9 @@ async def processing_coro(download_results_queue, parse_result_queue):
             results = await process_pool.coro_map(process_worker, entries_iter)
             for result in results:
                 if len(result) > 0:
-                    parse_result_queue.put(result)
+                    print('Adding {} Samples to MASS Queue...'.format(len(result)))
+                    for res in result:
+                        parse_result_queue.put(res)
 
         if done:
             break
@@ -173,7 +181,6 @@ async def processing_coro(download_results_queue, parse_result_queue):
 
 def process_worker(arg):
     result_info = arg[0]
-    timestamp = arg[1]
     parsed_results = []
 
     if not result_info:
@@ -186,29 +193,24 @@ def process_worker(arg):
             if mtl.LogEntryType == "X509LogEntryType":
                 cert_data['type'] = "X509LogEntry"
                 chain = [crypto.load_certificate(crypto.FILETYPE_ASN1, certlib.Certificate.parse(mtl.Entry).CertData)]
-                extra_data = certlib.CertificateChain.parse(base64.b64decode(entry['extra_data']))
-                for cert in extra_data.Chain:
-                    chain.append(crypto.load_certificate(crypto.FILETYPE_ASN1, cert.CertData))
             else:
                 cert_data['type'] = "PreCertEntry"
                 extra_data = certlib.PreCertEntry.parse(base64.b64decode(entry['extra_data']))
                 chain = [crypto.load_certificate(crypto.FILETYPE_ASN1, extra_data.LeafCert.CertData)]
-
-                for cert in extra_data.Chain:
-                    chain.append(
-                        crypto.load_certificate(crypto.FILETYPE_ASN1, cert.CertData)
-                    )
             cert_data.update({
                 "leaf_cert": certlib.dump_cert(chain[0]),
                 "chain": [certlib.dump_cert(x) for x in chain[1:]]
             })
-            if cert_data['leaf_cert']['not_before'] < timestamp:
-                continue
             certlib.add_all_domains(cert_data)
 
             cert_data['source'] = {
                 "url": result_info['log_info']['url'],
             }
+            for domain in cert_data['leaf_cert']['all_domains']:
+                if '.' in domain:
+                    break
+            else:
+                continue
 
             parsed_results.append({'log_url': result_info['log_info']['url'],
                                    'all_domains': cert_data['leaf_cert']['all_domains'],
@@ -224,7 +226,7 @@ def process_worker(arg):
     return parsed_results
 
 
-def submit_ctls_to_mass(urls, timestamp, anal_system_instance):
+def submit_ctls_to_mass(urls, anal_system_instance):
     logging.info("Submitting following CTLogs to MASS: {}".format(urls))
     for url in urls:
         for i in range(3):
@@ -232,7 +234,7 @@ def submit_ctls_to_mass(urls, timestamp, anal_system_instance):
                 s = Sample.create(domain=url, tags=['ctlog'])
                 scheduled = anal_system_instance.schedule_analysis(s)
                 scheduled.create_report(
-                    json_report_objects={'ctl_report': ('ctl_report', {'time': timestamp})},
+                    json_report_objects={'ctl_report': ('ctl_report', {'initial': True, 'offset': 0})},
                 )
                 break
             except requests.HTTPError:
@@ -240,8 +242,7 @@ def submit_ctls_to_mass(urls, timestamp, anal_system_instance):
                     logging.error('HTTPError while creating a CTL sample.')
 
 
-def get_ctls_from_mass(anal_system_instance):
-    new_time = time.time()
+def get_ctls_from_mass():
     dict = {}
     # TODO: BUG??? ctls = Sample.query(tags__all='ctlog')
     ctls = Sample.query()
@@ -249,12 +250,24 @@ def get_ctls_from_mass(anal_system_instance):
         if 'ctlog' in ctl.tags:
             # assumption: report[0] is the latest report
             report = ctl.get_reports()[0]
-            dict[ctl.unique_features.domain] = report.json_reports['ctl_report']['time']
-            scheduled = anal_system_instance.schedule_analysis(ctl)
-            scheduled.create_report(
-                json_report_objects={'ctl_report': ('ctl_report', {'time': new_time})},
-            )
+            dict[ctl.unique_features.domain] = {'initial': report.json_reports['ctl_report']['initial'], 'offset':report.json_reports['ctl_report']['offset']}
+
     return dict
+
+
+def create_ctl_report(anal_system_instance, domain, offset):
+    new_time = time.time()
+    ctls = Sample.query(domain=domain)
+
+    for ctl in ctls:
+        old = ctl.get_reports()[0].json_reports['ctl_report']['offset']
+        delta = offset - old
+
+        scheduled = anal_system_instance.schedule_analysis(ctl)
+        scheduled.create_report(
+            json_report_objects={'ctl_report': ('ctl_report', {'time': new_time, 'initial': False, 'offset': offset, 'delta': delta})},
+        )
+        break
 
 
 def main():
@@ -263,14 +276,13 @@ def main():
     ctl_urls = config.get('General', 'CT Logs')
     mass_concurrency = config.get('General', 'MASS concurrency')
     download_concurrency = config.get('General', 'download concurrency')
-    start_timestamp = config.get('General', 'start timestamp')
     crawl_once = config.getboolean('General', 'crawl once')
     time_sleep = config.get('General', 'time sleep')
     add_urls = config.getboolean('General', 'add CT Logs')
 
     loop = asyncio.get_event_loop()
 
-    parser = argparse.ArgumentParser(description='Pull down certificate transparency list information')
+    parser = argparse.ArgumentParser(description='Pull down certificate transparency list and store it in MASS.')
 
     parser.add_argument('-u', dest="ctl_urls", action="store_true", default=add_urls,
                         help="Retrieve the CTLs defined in config.ini additionally to CTLs stored in MASS")
@@ -280,9 +292,6 @@ def main():
 
     parser.add_argument('-m', dest='mass_concurrency', action='store', default=mass_concurrency, type=int,
                         help="The number of concurrent MASS submitter to run at a time")
-
-    parser.add_argument('-f', dest='start_timestamp', action='store', default=start_timestamp, type=int,
-                        help="Certificates with a SCT older than this value are ignored.")
 
     parser.add_argument('-o', dest='crawl_once', action='store_true', default=crawl_once,
                         help='Crawl only once.')
@@ -304,7 +313,7 @@ def main():
                                                                   )
 
     if args.ctl_urls:
-        submit_ctls_to_mass(ctl_urls.replace(' ', '').split(','), args.start_timestamp, anal_system_instance)
+        submit_ctls_to_mass(ctl_urls.replace(' ', '').split(','), anal_system_instance)
 
     loop.run_until_complete(
         retrieve_certificates(loop, download_concurrency=args.download_concurrency, anal_system_instance=anal_system_instance,
